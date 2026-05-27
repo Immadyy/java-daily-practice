0efdcaab63c927f4c942ceddd53844dc819345e9
Immady <ahmadali63288@gmail.com>
2026-05-27 17:40:47 +0530

    add file Ep03 Collection (List, map, hashmap, set, hashset, array) && comment out every file code

---
+++ src/Ep03.java
@@ -0,0 +1,31 @@
+//public class Ep03 {
+//    static void main() {
+            //List && ArrayList
+//        List<String> arr = new ArrayList<>();
+//        arr.add("Hello");
+//        arr.add("World");
+//        for (int i = 0; i < arr.size(); i++) {
+//            System.out.println(arr.get(i));
+//        }
+
+//            //Array
+//        String[] arr = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+//        for (String day : arr) {
+//            System.out.println(day);
+//        }
+
+            // set
+//        Set<String> days =  new HashSet<>(List.of("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"));
+//            for (String val : days) {
+//                System.out.println(val);
+//            }
+
+            //Maps
+//        Map<String, Integer> marks = new HashMap<>();
+//        marks.put("Rohan", 80);
+//        marks.put("Pushpa", 100);
+//        for (Map.Entry<String, Integer> entry : marks.entrySet() ) {
+//            System.out.println("Student: " + entry.getKey() + ", Marks: " + entry.getValue());
+//        }
+//    }
+//}
